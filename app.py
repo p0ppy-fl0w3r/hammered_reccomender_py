05fc06f56c5bf78cf64c6135ac443de34b4f274e
p0ppy-fl0w3r <kunai_madara@protonmail.com>
2022-01-06 22:35:10 +0545

    Added ratings to data and re-factored code.

--- app.py
+++ app.py
@@ -1,10 +1,41 @@
-import json
-
-import constants
+import numpy
 
 from cocktail_data import CocktailData
+from constants import DATA_FILE
 
-cocktail_list = CocktailData.load_data(constants.DATA_FILE)
+user_cocktail = CocktailData.load_data(DATA_FILE)
 
-for i in cocktail_list:
-    print(i)
+# General recommendation
+
+
+def general_recommendation():
+    # Sort the list by highest rating.
+    user_cocktail.sort(key=lambda c: c.rating, reverse=True)
+
+    sample_cocktail = user_cocktail[:10]
+
+    ing_list = []
+    for i in sample_cocktail:
+        ing_list += i.ingredients
+
+    ing_set = set(ing_list)
+
+    rating_ingredient_list = []
+    for i in sample_cocktail:
+        contains_ingredient = [0 for _ in range(len(ing_set))]
+        for j in enumerate(ing_set):
+            if j[1] in i.ingredients:
+                contains_ingredient[j[0]] = 1*i.rating
+        rating_ingredient_list.append(contains_ingredient)
+
+    sum_arr = numpy.sum(rating_ingredient_list, axis=0)
+
+    sum_rating = numpy.sum(sum_arr)
+
+    weighted_ingredient_dict = dict()
+    for ingredient, init_rating in zip(ing_set, sum_arr):
+        weighted_ingredient_dict[ingredient] = init_rating/sum_rating
+
+    print(weighted_ingredient_dict)
+
+general_recommendation()
--- cocktail_data.py
+++ cocktail_data.py
@@ -4,6 +4,10 @@ from constants import COCKTAIL, INGREDIENT, RATING
 
 
 class CocktailData:
+    """
+    A representation of Cocktail with ingredients and rating.
+    """
+
     def __init__(self, cocktail, ingredients, rating):
         self.cocktail = cocktail
         self.ingredients = ingredients
@@ -11,19 +15,40 @@ class CocktailData:
 
     @staticmethod
     def from_json(json_data):
-        return CocktailData(json_data[COCKTAIL],
-                            json_data[INGREDIENT],
-                            json_data[RATING])
+        """
+        Creates a CocktailData using the given argument.
+
+        Args:
+            json_data (dict): A dictionary of raw cocktail data.
+
+        Returns:
+            CocktailData
+        """
+
+        return CocktailData(
+            json_data[COCKTAIL], json_data[INGREDIENT], json_data[RATING]
+        )
 
     @staticmethod
     def load_data(data_file):
-        with open(data_file, 'r') as input_file:
+        """
+        Creates a list of CocktailData using the raw json data of the
+        given file.
+
+        Args:
+            data_file (str): Filename of the file containing the raw json
+
+        Returns:
+            List<CocktailData>
+        """
+
+        with open(data_file, "r") as input_file:
             j_data = json.load(input_file)
 
         cocktail_list = []
         for j_cocktail in j_data:
-            cocktail_list .append(CocktailData.from_json(j_cocktail))
-        
+            cocktail_list.append(CocktailData.from_json(j_cocktail))
+
         return cocktail_list
 
     def __str__(self):
